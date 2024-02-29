eb897ffbdaf6bdbcb3d1045d90bdcb651bb82647
Travis <you@example.com>
2024-02-28 21:29:10 -0500

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import routes from './config/routes';
 import Navbar from './components/Navbar';
 import { Provider } from 'react-redux';
-import { store } from './redux/store'
+import { store } from './redux/store';
+
+import AuthChecker from './auth/AuthChecker';
 
 
 function App() {
@@ -17,7 +19,13 @@ function App() {
                 key={index}
                 path={route.path}
                 element={
+                  route.protected ? (
+                  <AuthChecker>
                   <route.component />
+                  </AuthChecker>
+                  ) : (
+                  <route.component />
+                  )
                 }
                 />
             )) }
---
+++ src/config/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAbnRVRZHlYcOzMnGwjYdr9O6NdOiEh3bY",
+  authDomain: "fs-phonebook.firebaseapp.com",
+  projectId: "fs-phonebook",
+  storageBucket: "fs-phonebook.appspot.com",
+  messagingSenderId: "277816162234",
+  appId: "1:277816162234:web:6cf4f0585c1fb513718f83",
+  measurementId: "G-7JEGRFD6BW"
+};
